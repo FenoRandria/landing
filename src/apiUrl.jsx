2f62aa85eb0270e3d48d0db60788fde71bcd2235
FenoRandria <117942753+FenoRandria@users.noreply.github.com>
2024-07-12 17:27:47 +0300

    remplacement element

--- src/apiUrl.jsx
+++ src/apiUrl.jsx
@@ -1,4 +1,4 @@
-const apiUrl = 'https://projectclouds5backend-production.up.railway.app';
-// const apiUrl = 'http://localhost:2191';
+// const apiUrl = 'https://projectclouds5backend-production.up.railway.app';
+const apiUrl = 'http://localhost:2191';
 export default apiUrl;
 
